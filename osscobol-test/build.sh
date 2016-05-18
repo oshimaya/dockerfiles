c986c3791eaccc60a4f5d630431059687d85b462
Yasushi Oshima <oshima@cent7host.yagoto-urayama.jp>
2016-05-18 23:30:06 +0900

    refuctor build stript.
     - Get distribution tarball from web site when any tarballs
       do not exist in /files
     - change build.sh path to /root
     - split phase:
       1. extract taball
       2. apply patches when patch subcommand
       3. build
       4. test in src/tests directory
     - sub command:
       build: extract tarball and build
       patch: extract tarball and patch from /files/patch/distribution/*.patch
       test:  extract taball and build and run test
       patchtest: extract taball and patch and run test

--- osscobol-test/build.sh
+++ osscobol-test/build.sh
@@ -1,25 +1,121 @@
 #!/bin/bash
-if [ "$1" == "build" ] ; then
-for i in $(ls /files/opensource-cobol*.tar.gz 2> /dev/null)
-do
-  BASE=${i#/files/}
-  BASE=${BASE%.tar.gz}
-  tar zxpf $i 
-done
-for i in $(ls -d opensource-cobol*)
-do
-  if `echo $i | grep -q utf8` ; then
-    OPT="--enable-utf8"
-  else
-    OPT=""
-  fi
-  cd $i
-  ./configure $OPT
-  make
-  cd tests
-  ./run
-  cd ../..
-done
-fi
+
+BASEURL="http://www.osscons.jp/osscobol/files/"
+DEFAULTFILEPATH="?action=cabinet_action_main_download&block_id=414&room_id=21&cabinet_id=11&file_id=362&upload_id=704"
+DEFAULTFILE=opensource-cobol-1.5.0J-utf8.tar.gz
+
+applypatch(){
+    local target patchfile
+    target=$1
+    if [ -d /files/patches/$target ]; then
+        if [ -d /work/$target ]; then
+            ( cd /work/$target
+            for patchfile in $(ls /files/patches/${target}/*.patch 2> /dev/null)
+            do
+	        patch -p0 <  $patchfile
+            done
+            )
+        fi
+    fi
+}
+
+extractall(){
+    local tarball base
+    for tarball in $(ls /files/opensource-cobol*.tar.gz 2> /dev/null)
+    do 
+        tar zxf $tarball -C /work
+    done 
+}
+
+dobuild(){
+    local target opt
+    target=$1
+    if (echo $target | grep -q 'utf8$'); then
+        opt="--enable-utf8"
+    else
+        opt=""
+    fi
+    cd /work/${target}
+    ./configure $opt
+    make
+}
+
+dotestsh(){
+    local target opt testsh
+    target=$1
+    if (echo $target | grep -q 'utf8$'); then
+        opt="utf8"
+    else
+        opt="sjis"
+    fi
+    cd /work/${target}/tests
+    for testsh in data-rep-O i18n_${opt} jp-compat run-O syntax
+    do
+        sh ./${testsh}
+    done
+    for testsh in data-rep run
+    do
+	mv ${testsh}.dir ${testsh}-O.dir
+	mv ${testsh}.log ${testsh}-O.log
+        sh ./${testsh}
+    done
+}
+
+getdistribution() {
+    curl -L -o /files/${DEFAULTFILE} ${BASEURL}${DEFAULTFILEPATH}
+}
+
+extract() {
+    if [ ! $(ls -d /files/opensource-cobol*.tar.gz 2>/dev/null) ]; then
+        getdistribution
+    fi
+    extractall
+}
+
+patchall() {
+    local dir
+    for dir in $(ls -d /work/opensource-cobol*)
+    do
+        base=${dir#/work/}
+        applypatch $base
+    done
+}
+
+buildall() {
+    local dir flag
+    flag=$1
+    for dir in $(ls -d /work/opensource-cobol*)
+    do
+        base=${dir#/work/}
+        dobuild $base
+        if [ $? -eq 0 ]; then
+            if [ "$flag" = "dotest" ]; then
+                dotestsh $base
+            fi
+        fi
+    done
+}
+
+
+case $1 in
+patchbuild)
+    extract && patchall && buildall
+    ;;
+build)
+    extract && buildall
+    ;;
+patch)
+    extract && patchall 
+    ;;
+test)
+    extract && buildall dotest
+    ;;
+patchtest)
+    extract && patchall && buildall dotest
+    ;;
+*)
+    echo "Enter shell"
+    ;;
+esac
 /bin/bash
 
